5c334e91e00e421f6d74ea60fd3928a213474b26
Sofia <soffest@soffest-PC.(none)>
2014-03-12 15:35:32 +0400

    separate folders, mouseover and mouseout deleted after shot

---
+++ js/cell.js
@@ -0,0 +1,116 @@
+seaBattle.Cell = function (x, y) {
+    var empty = 'empty',
+		miss = 'miss',
+		ship = 'ship-horizontal',
+		shipBegin = 'ship-begin-horizontal',
+		shipEnd = 'ship-end-horizontal',
+		ship = 'ship-vertical',
+		shipBegin = 'ship-begin-vertical',
+		shipEnd = 'ship-end-vertical',
+		shipCrashed = 'ship-crashed',
+		sank = 'sank',
+		underCursor = 'under-cursor';
+
+	var handlerClick = null,
+		handlerMouseOver = null,
+		handlerMouseOut = null;
+				
+    
+    this.x = x;
+    this.y = y;
+    this.state = empty;
+    this.ship = null;
+    this.previousClassName = 'empty';
+	this.td = null;
+	this.field = null;
+	
+	
+	
+    this.setClassName = function (state) {
+        this.td.className = (state == 'sank') ? state : ('standart-cell '+ state);   
+    }
+    
+    this.createElement = function (attachment, randomly) {
+        this.td = document.createElement('td');
+        this.td.setAttribute('data-x', this.x);
+        this.td.setAttribute('data-y', this.y);
+        
+        //Enemy`s Field. Click means shot.
+		if (attachment == 'enemyField') {
+			this.td.className = 'standart-cell '+'empty';
+			handlerClick = this.shoot.bind(this);
+			handlerMouseOver = this.hover.bind(this);
+			handlerMouseOut = this.returnState.bind(this);
+			this.td.addEventListener('click', handlerClick );
+			this.td.addEventListener('mouseover', handlerMouseOver );
+			this.td.addEventListener('mouseout', handlerMouseOut);
+		}
+		else {
+			// Player`s field. Clicks are used to arrange ships 
+			if (randomly) {
+				this.td.className = 'standart-cell '+this.state;
+			}
+		//If player chose random arrangement of his ships
+			else {
+				this.td.className = 'standart-cell '+this.state;
+				handlerClick = this.clicked.bind(this);
+				this.td.addEventListener('click', handlerClick, true );
+				//this.td.addEventListener('mouseover', this.hover.bind(this));
+				//this.td.addEventListener('mouseout',this.returnState.bind(this));
+			}
+		}
+        return this.td;
+    };
+	
+	this.clicked = function () {
+		this.field.arrangeShip(this.x, this.y);
+	}
+			                            
+    this.shoot = function (event) {
+		var shotResult = this.checkShot();
+		this.delEvent('click', handlerClick);
+		this.delEvent('mouseover', handlerMouseOver);
+		this.delEvent('mouseout', handlerMouseOut);
+		seaBattle.theGame.priority(shotResult);
+	}
+
+	this.setShip = function () {
+		
+	}
+
+	this.delEvent = function (event,h) {
+		this.td.removeEventListener(event, h );
+	}
+	
+ 
+    this.hover = function (event) {
+        this.setClassName(underCursor);
+	}
+    
+    this.returnState = function (event) {
+        this.setClassName(this.previousClassName);
+    }
+	
+	
+	
+	//cheks shot result
+	this.checkShot = function () {
+		if (this.state == 'empty') {
+					this.state ='miss';
+		}
+		else {
+			this.ship.lives --;
+			if (this.ship.lives == 0) {
+				this.ship.draw ('sank');
+				this.state = 'sank';
+			}
+			else {
+				this.state = 'ship-crashed';
+			}
+		}
+		this.setClassName(this.state);
+		this.previousClassName = this.state;
+		return this.state;
+	}	
+}
+
---
+++ js/ship.js
@@ -0,0 +1,45 @@
+seaBattle.Ship = function (size) {
+	this.size = size;
+	this.orientation;//horizontal or vertical
+	this.decks = [];
+	this.lives = size;
+
+	for (var j = 0; j < this.size; j++) {
+     this.decks.push(null);        
+		}
+	this.draw = function (className) {
+		for (var k = 0; k< this.size; k++) {
+			if (className) {
+				this.decks[k].state = className;
+				this.decks[k].setClassName(className);
+				this.decks[k].previousClassName = className;
+			}
+			else {
+				this.decks[k].setClassName(this.decks[k].state);
+				this.decks[k].previousClassName = this.decks[k].state;
+			}
+			//alert ('className='+className);
+		}
+	} 
+	this.makeState = function (deckNumber) {
+		/*if (deckNumber>0) {
+			if (deckNumber == this.size-1) {
+				state ='ship-end' 
+				}
+				else {
+				state = 'ship' 
+				}
+		}
+		else {
+		state = 'ship-begin';	
+		}*/
+		state = (deckNumber == 0) ? 'ship-begin' : (deckNumber == this.size-1) ? 'ship-end' : 'ship';
+		state = state + '-' + this.orientation;
+		this.decks[deckNumber].state = state;
+		return state; 
+	}
+	
+	
+}
+
+
